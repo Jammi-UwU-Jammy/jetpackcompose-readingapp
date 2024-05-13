fa257b5e79e99ba34c56257858f5375d7373a572
Jammi-UwU-Jammy <jamclark102@gmail.com>
2024-05-13 01:44:15 -0400

    changed searchbar

--- app/src/main/java/com/vivich/starlitapp/ui/lobby/LobbyTopBar.kt
+++ app/src/main/java/com/vivich/starlitapp/ui/lobby/LobbyTopBar.kt
@@ -1,30 +1,39 @@
 package com.vivich.starlitapp.ui.lobby
 
-import android.util.Log
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.DrawerState
+import androidx.compose.material.icons.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.TextField
+import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LobbyTopNav(
     openDrawer: () -> Unit
 ){
+    var text by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+
     NavigationBar {
         NavigationBarItem(
             selected = false,
@@ -33,25 +42,35 @@ fun LobbyTopNav(
                 Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
             }
         )
-
         NavigationBarItem(
             modifier = Modifier.weight(2.0f),
             selected = false,
             onClick = { /*TODO*/ },
             icon = {
                 Row(
-                    modifier = Modifier.padding(20.dp, 5.dp)
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    TextField(
-                        modifier = Modifier.weight(0.5f),
-                        value = TextFieldValue("Some text"),
-                        onValueChange = {}
-                    )
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "Find something"
-                        )
+                    SearchBar(
+                        query = text,
+                        onQueryChange = { text = it },
+                        onSearch = { active = false },
+                        active = active,
+                        onActiveChange = {
+                            active = it
+                        }
+                    ) {
+                        // Search result shown when active
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentPadding = PaddingValues(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ) {
+                            items(4) { idx ->
+                                // Search result
+
+                            }
+                        }
+
                     }
                 }
             }
@@ -64,4 +83,25 @@ fun LobbyTopNav(
             }
         )
     }
+}
+
+
+@Preview
+@Composable
+fun TopBarPreview(){
+    LobbyTopNav {
+
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomSearchBar(
+
+){
+    var text by rememberSaveable { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+
 }
